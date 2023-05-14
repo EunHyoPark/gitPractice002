042370f4ac6b9d9281fbb3adb6c291424d4e80aa
Sun-JiHoon <cangogi1@naver.com>
2023-05-14 11:40:47 +0900

    selec.c 수정

--- Project1/Project1/select.c
+++ Project1/Project1/select.c
@@ -5,6 +5,7 @@
 int slec() {
 	int num = 0;
 	printf("원하는 기능 숫자를 입력하시고 enter 후 두 숫자를 입력해주세요.");
+	printf("ex) 1 enter 10 50 => 결과값");
 	printf("1. 더하기\n");
 	printf("2. 빼기\n");
 	printf("3. 곱하기\n");
